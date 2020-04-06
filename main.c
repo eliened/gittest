21b1be6017d244ca45f91bc0f488ecde8de5e04a
Elie Nedjar <thelilou21@gmail.comr>
2020-04-06 15:43:29 +0300

    implant sum dans code

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
+int sum(int a, int b);
+
 int main() {
     printf("Hello, World!\n");
     printf("Hello teub");
     printf("Apprendre git");
+    int s = sum(3, 5);
+    printf("\n %d",s);
     return 0;
 }
 
 
 int sum(int a, int b){
     return a + b;
+
 }
+
