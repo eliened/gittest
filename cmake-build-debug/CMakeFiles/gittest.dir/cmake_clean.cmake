b23dcb45273ecf4f681874e801a96ecd1c9e34f3
Elie Nedjar <thelilou21@gmail.comr>
2020-04-06 11:22:54 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/gittest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gittest.dir/main.c.o"
+  "gittest"
+  "gittest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gittest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
